350bc543254d4214797327c61dab6b13bfa17930
arpachuilo <andrew.pachuilo@gmail.com>
2015-10-14 09:11:19 -0500

    Got some buildings and terrain

---
+++ Assets/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	
+	public float tumble;
+	
+	void Start() {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
